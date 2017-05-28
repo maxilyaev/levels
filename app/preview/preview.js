acb201654441ef880d42adfb635fc0aa23882dc4
maxilyaev <maxilyaev@yahoo.com>
2017-05-28 15:25:32 +0300

    Add files via upload

---
+++ app/preview/preview.js
@@ -0,0 +1,53 @@
+(function() {
+  // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+  // e.g. just plyr.setup(); and leave it at that if you have no need for events
+  var instances = plyr.setup({
+    // Output to console
+    debug: true,
+    controls: [],
+    //autoplay: true,
+    showPosterOnEnd: true
+  });
+
+  // Get an element
+  function get(selector) {
+    return document.querySelector(selector);
+  }
+
+  // Custom event handler (just for demo)
+  function on(element, type, callback) {
+    if (!(element instanceof HTMLElement)) {
+      element = get(element);
+    }
+    element.addEventListener(type, callback, false);
+  }
+
+  // Loop through each instance
+  instances.forEach(function(instance) {
+    // Play
+    on('.js-play', 'click', function() {
+      instance.play();
+    });
+
+    // Pause
+    on('.js-pause', 'click', function() {
+      instance.pause();
+    });
+
+    // Stop
+    on('.js-stop', 'click', function() {
+      //instance.stop();
+      instance.getMedia().load();
+    });
+
+    // Rewind
+    on('.js-rewind', 'click', function() {
+      instance.rewind();
+    });
+
+    // Forward
+    on('.js-forward', 'click', function() {
+      instance.forward();
+    });
+  });
+})();
